b5b53edbbda7477bd5893f10feee15fd30c41066
Michele Segata <msegata@disi.unitn.it>
2012-08-10 18:38:25 +0200

    working puncturing

--- src/ofdm_utils.c
+++ src/ofdm_utils.c
@@ -19,6 +19,8 @@
  *
  */
 
+#include <assert.h>
+
 #include "ofdm_utils.h"
 #include "bit_utils.h"
 
@@ -71,50 +73,121 @@ void convolutional_encoding(const char *in, char *out_a, char *out_b, int size) 
 
         for (ib = 7; ib >= 0; ib--) {
 
-
-
         }
 
     }
 
 }
-#include <stdio.h>
+
 void pucturing(const char *in_a, const char *in_b, char *out, int size, enum CODING_RATE rate) {
 
-    int i, ib;
+    int i;
     //size of the out array
     int out_size;
 
+    //iterators for in_a, in_b and out
+    int i_a, i_b, i_o;
+    /**
+     * mod for in_a and in_b, i.e., when bit must be drop.
+     * e.g.,
+     * set to 0 = drop no bits
+     * set to 1 = drop every bit
+     * set to 2 = drop one every two
+     */
+    int mod_a, mod_b;
+    /**
+     * drop shifter for in_a and in_b. might be needed if you
+     * want to drop one every two but start dropping from the
+     * first
+     */
+    int shift_a, shift_b;
+
     switch (rate) {
     case RATE_1_2:
+
+        mod_a = 0;
+        mod_b = 0;
+        shift_a = 0;
+        shift_b = 0;
+
         out_size = size * 2;
+
         break;
     case RATE_2_3:
+
+        //every bit from a
+        mod_a = 0;
+        //one bit every two from bit
+        mod_b = 2;
+        //no shifts
+        shift_a = 0;
+        //start dropping from second bit
+        //1, 0, 1, 0, 1, 0
+        shift_b = 1;
+
         out_size = size * 3 / 2;
+
         break;
     case RATE_3_4:
+
+        //two bits every three from a
+        mod_a = 3;
+        //two bits every three from b
+        mod_b = 3;
+        //start dropping from the third
+        //1, 2, 0, 1, 2, 0
+        shift_a = 1;
+        //start dropping from the second bit
+        //2, 0, 1, 2, 0
+        shift_b = 2;
+
         out_size = size * 4 / 3;
+
         break;
     default:
-        //TODO add assert
-        return;
+        assert(0);
         break;
     }
 
-    for (i = 0; i < size; i++) {
+    i_a = 0;
+    i_b = 0;
+    i_o = 0;
 
-        for (ib = 7; ib >= 0; ib--) {
+    //cycle until we filled up the output array
+    for (i = 0; i_b < size * 8; i++) {
 
-            //coding rate 1/2 just takes all the bits of the input
+        if (i % 2 == 0) {
+            //we are considering bit of in_a
+            if (mod_a == 0 || (i_a + shift_a) % mod_a != 0) {
+                //bit must not be dropped
+                //printf("out[%d][%d] = in_a[%d][%d]\n", i_o / 8, 7 - i_o % 8, i_a / 8, 7 - i_a % 8);
+                set_bit(&out[i_o / 8], 7 - i_o % 8, get_bit(in_a[i_a / 8], 7 - (i_a % 8)));
 
-            int bit_a = get_bit(in_a[i], ib);
-            int bit_b = get_bit(in_b[i], ib);
-            //printf("i0 = %d, i = %d, ib1 = %d, ib2 = %d\n", i, i * 2  + ib * 2 / 8, ib * 2 % 8, ib * 2 % 8 + 1);
-            set_bit(&out[i * 2 + ib * 2 / 8], ib * 2 % 8 + 1, bit_a);
-            set_bit(&out[i * 2 + ib * 2 / 8], ib * 2 % 8, bit_b);
+                //bit not dropped, so jump to next output bit
+                i_o++;
+            }
+            //increase index for in_a
+            i_a++;
+        } else {
+            //we ar//we are considering bit of in_b
+            if (mod_b == 0 || (i_b + shift_b) % mod_b != 0) {
+                //bit must not be dropped
+                //printf("out[%d][%d] = in_b[%d][%d]\n", i_o / 8, 7 - i_o % 8, i_b / 8, 7 - i_b % 8);
+                set_bit(&out[i_o / 8], 7 - i_o % 8, get_bit(in_b[i_b / 8], 7 - (i_b % 8)));
 
+                //bit not dropped, so jump to next output bit
+                i_o++;
+            }
+            //increase index for in_b
+            i_b++;
         }
 
     }
+
+    //printf("ia=%d ib=%d io=%d size=%d outsize=%d\n", i_a, i_b, i_o, size * 8, out_size * 8);
+
+    assert(i_a == size * 8);
+    assert(i_b == size * 8);
+    assert(i_o == out_size * 8);
 
 }
