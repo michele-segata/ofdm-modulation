44ceb3afc291a086696382d8ade0f247c0f47ecb
Michele Segata <msegata@disi.unitn.it>
2012-08-10 18:38:41 +0200

    puncturing tester

--- test/ofdm_puncturing.c
+++ test/ofdm_puncturing.c
@@ -2,49 +2,55 @@
 
 #include "ofdm_utils.h"
 
+#define SIZE 24
+
 int main() {
 
-	char in_a[8];
-	char in_b[8];
-	char out[16];
-
-	in_a[0] = 0x00;
-	in_a[1] = 0x00;
-	in_a[2] = 0x00;
-	in_a[3] = 0x00;
-	in_a[4] = 0x00;
-	in_a[5] = 0x00;
-	in_a[6] = 0x00;
-	in_a[7] = 0x00;
-
-	in_b[0] = 0xFF;
-	in_b[1] = 0xFF;
-	in_b[2] = 0xFF;
-	in_b[3] = 0xFF;
-	in_b[4] = 0xFF;
-	in_b[5] = 0xFF;
-	in_b[6] = 0xFF;
-	in_b[7] = 0xFF;
+	char in_a[SIZE];
+	char in_b[SIZE];
+	char out[SIZE*2];
 
 	int i;
+
+	for (i = 0; i < SIZE; i++) {
+	    in_a[i] = 0x00;
+	    in_b[i] = 0xFF;
+	}
+
 	printf("Input bit sequence A:\n");
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < SIZE; i++) {
 		print_bits(in_a[i]);
 	}
 	printf("\n");
 	printf("Input bit sequence B:\n");
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < SIZE; i++) {
 	    print_bits(in_b[i]);
 	}
 	printf("\n");
 
-	pucturing(in_a, in_b, out, 8, RATE_1_2);
+	pucturing(in_a, in_b, out, SIZE, RATE_1_2);
 
 	printf("Punctured bit sequence r=1/2:\n");
-	for (i = 0; i < 16; i++) {
+	for (i = 0; i < SIZE * 2 ; i++) {
 		print_bits(out[i]);
 	}
 	printf("\n");
+
+    pucturing(in_a, in_b, out, SIZE, RATE_3_4);
+
+    printf("Punctured bit sequence r=3/4:\n");
+    for (i = 0; i < SIZE * 4 / 3; i++) {
+        print_bits(out[i]);
+    }
+    printf("\n");
+    pucturing(in_a, in_b, out, SIZE, RATE_2_3);
+
+    printf("Punctured bit sequence r=2/3:\n");
+    for (i = 0; i < SIZE * 3 / 2; i++) {
+        print_bits(out[i]);
+    }
+    printf("\n");
+
 
 	return 0;
 
