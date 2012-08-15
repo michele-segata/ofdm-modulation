3ce98d7220fde844c19221716b1eb4a9798a4c21
Michele Segata <msegata@disi.unitn.it>
2012-08-15 23:37:04 +0200

    fix scrambler, encoder and puncturer test

--- test/ofdm_scrambler.c
+++ test/ofdm_scrambler.c
@@ -4,33 +4,57 @@
 
 int main() {
 
-	char in[8];
-	char out[8];
+#define SIZE 12
 
-	in[0] = 0x00;
-	in[1] = 0x00;
-	in[2] = 0x20;
-	in[3] = 0x40;
-	in[4] = 0x00;
-	in[5] = 0x74;
-	in[6] = 0x00;
-	in[7] = 0x06;
+    char in[SIZE];
+    char out[SIZE*2];
+    char out_a[SIZE];
+    char out_b[SIZE];
+    char tmp[SIZE*2];
 
-	int i;
-	printf("Input bit sequence:\n");
-	for (i = 0; i < 8; i++) {
-		print_bits(in[i]);
-	}
-	printf("\n");
+    in[0]  = 0x00;
+    in[1]  = 0x00;
+    in[2]  = 0x20;
+    in[3]  = 0x40;
+    in[4]  = 0x00;
+    in[5]  = 0x74;
+    in[6]  = 0x00;
+    in[7]  = 0x06;
+    in[8]  = 0x08;
+    in[9]  = 0xB3;
+    in[10] = 0xEC;
+    in[11] = 0x65;
 
-	scramble(in, out, 8);
+    int i;
+    printf("Input bit sequence:\n");
+    for (i = 0; i < SIZE; i++) {
+        print_bits(in[i]);
+    }
+    printf("\n");
 
-	printf("Scrambled bit sequence:\n");
-	for (i = 0; i < 8; i++) {
-		print_bits(out[i]);
-	}
-	printf("\n");
+    scramble_with_initial_state(in, out, SIZE, 0x5D);
 
-	return 0;
+    printf("Scrambled bit sequence:\n");
+    for (i = 0; i < SIZE; i++) {
+        print_bits(out[i]);
+    }
+    printf("\n");
+
+    convolutional_encoding(out, tmp, SIZE);
+    printf("Encoded bit sequence rate 1/2:\n");
+    for (i = 0; i < SIZE * 2; i++) {
+        print_bits(tmp[i]);
+    }
+    printf("\n");
+
+    pucturing(tmp, out, SIZE * 2, RATE_3_4);
+
+    printf("Punctured bit sequences:\n");
+    for (i = 0; i < SIZE * 2 * 2 / 3; i++) {
+        print_bits(out[i]);
+    }
+    printf("\n");
+
+    return 0;
 
 }
