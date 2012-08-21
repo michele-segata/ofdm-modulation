a64dfbc3f09ebde2faedd001c26427bc40cc7b97
Michele Segata <segata@ccs.uibk.ac.at>
2012-08-21 18:49:27 +0200

    add modulation step to tester

--- test/ofdm_scrambler.c
+++ test/ofdm_scrambler.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <fftw3.h>
 
 #include "ofdm_utils.h"
 #include "bit_utils.h"
@@ -10,6 +13,10 @@ int main() {
     char in[SIZE];
     char out[SIZE * 2];
     char tmp[SIZE * 2];
+    double **mod;
+    fftw_complex * ifft = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * 64);;
+
+    struct OFDM_PARAMETERS params = get_ofdm_parameter(BW_20_DR_36_MBPS);
 
     int read_bytes = read_bits_from_file("misc/data.bits", in, SIZE);
 
@@ -25,6 +32,12 @@ int main() {
     printf("Read %d bytes = %d bits\n", read_bytes, read_bytes * 8);
 
     int i;
+
+    mod = (double **) calloc(48, sizeof(double *));
+    for (i = 0; i < 48; i++) {
+        mod[i] = (double *) calloc(2, sizeof(double *));
+    }
+
     printf("Input bit sequence:\n");
     for (i = 0; i < SIZE; i++) {
         print_bits(in[i]);
@@ -46,7 +59,7 @@ int main() {
     }
     printf("\n");
 
-    pucturing(tmp, out, SIZE * 2, RATE_3_4);
+    pucturing(tmp, out, SIZE * 2, params.coding_rate);
 
     printf("Punctured bit sequence rate 3/4:\n");
     for (i = 0; i < SIZE * 2 * 2 / 3; i++) {
@@ -54,13 +67,27 @@ int main() {
     }
     printf("\n");
 
-    interleave(out, tmp, SIZE * 2 * 2 / 3, 192, 4);
+    interleave(out, tmp, SIZE * 2 * 2 / 3, params.n_cbps, params.n_bpsc);
 
     printf("Interleaved bit sequence:\n");
     for (i = 0; i < SIZE * 2 * 2 / 3; i++) {
         print_bits(tmp[i]);
     }
     printf("\n");
+
+    modulate(tmp, SIZE * 2 * 2 / 3, params.data_rate, mod);
+    map_ofdm_to_ifft(mod, ifft);
+
+    printf("Modulated bits (QAM16):\n");
+    for (i = 0; i < 64; i++) {
+        printf("(%.4f, %.4f) ", ifft[i][0], ifft[i][1]);
+    }
+    printf("\n");
+
+    for (i = 0; i < 48; i++) {
+        free(mod[i]);
+    }
+    free(mod);
 
     return 0;
 
